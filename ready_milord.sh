39d20f4452e38a543663307532156061089ea29d
Kim Ahlström <kim.ahlstrom@gmail.com>
2012-07-31 11:23:04 +0900

    Add support for terminal-notifier on 10.8

--- ready_milord.sh
+++ ready_milord.sh
@@ -2,10 +2,17 @@
 
 # AppleScript from http://www.devdaily.com/mac-os-x/applescript-unix-mac-osx-foreground-application-result
 
+# TODO: Put osascript part in actual executable and & it to make it run in background and avoid osascript slowness
+
 function ready_milord {
   APP=`osascript -e 'tell application "System Events"' -e 'set frontApp to name of first application process whose frontmost is true' -e 'end tell' 2>/dev/null`
   if [ "$APP" != 'Terminal' ]; then
-    growlnotify -m "Sayeth the Terminal: I'm ready for your next command, milord."
+    message="Sayeth the Terminal: I'm ready for your next command, milord."
+    if [ -d "/Applications/terminal-notifier.app/" ]; then
+      /Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier -message "$message" 1>/dev/null
+    else
+      growlnotify -m "$message"
+    fi
   fi
 }
 
